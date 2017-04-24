c39217420670df92f4c4eae7c02aabe34e27eb98
nanb2016 <nancybai@stanford.edu>
2017-04-23 22:12:35 -0700

    Add parser

--- parser/include/ast-parse.h
+++ parser/include/ast-parse.h
@@ -1,5 +1,5 @@
-#ifndef _COOL_PARSE_H
-#define _COOL_PARSE_H
+#ifndef _AST_PARSE_H
+#define _AST_PARSE_H
 //
 // See copyright.h for copyright notice and limitation of liability
 // and disclaimer of warranty provisions.
@@ -83,71 +83,88 @@ typedef Cases_class *Cases;
    /* Put the tokens into the symbol table, so that GDB and other debuggers
       know about them.  */
    enum yytokentype {
-     CLASS = 258,
-     ELSE = 259,
-     FI = 260,
-     IF = 261,
-     IN = 262,
-     INHERITS = 263,
-     LET = 264,
-     LOOP = 265,
-     POOL = 266,
-     THEN = 267,
-     WHILE = 268,
-     CASE = 269,
-     ESAC = 270,
-     OF = 271,
-     DARROW = 272,
-     NEW = 273,
-     ISVOID = 274,
-     STR_CONST = 275,
-     INT_CONST = 276,
-     BOOL_CONST = 277,
-     TYPEID = 278,
-     OBJECTID = 279,
-     ASSIGN = 280,
-     NOT = 281,
-     LE = 282,
-     ERROR = 283,
-     LET_STMT = 285
+     PROGRAM = 258,
+     CLASS = 259,
+     METHOD = 260,
+     ATTR = 261,
+     FORMAL = 262,
+     BRANCH = 263,
+     ASSIGN = 264,
+     STATIC_DISPATCH = 265,
+     DISPATCH = 266,
+     COND = 267,
+     LOOP = 268,
+     TYPCASE = 269,
+     BLOCK = 270,
+     LET = 271,
+     PLUS = 272,
+     SUB = 273,
+     MUL = 274,
+     DIVIDE = 275,
+     NEG = 276,
+     LT = 277,
+     EQ = 278,
+     LEQ = 279,
+     COMP = 280,
+     INT = 281,
+     STR = 282,
+     BOOL = 283,
+     NEW = 284,
+     ISVOID = 285,
+     NO_EXPR = 286,
+     OBJECT = 287,
+     NO_TYPE = 288,
+     STR_CONST = 289,
+     INT_CONST = 290,
+     ID = 291,
+     LINENO = 292
    };
 #endif
 /* Tokens.  */
-#define CLASS 258
-#define ELSE 259
-#define FI 260
-#define IF 261
-#define IN 262
-#define INHERITS 263
-#define LET 264
-#define LOOP 265
-#define POOL 266
-#define THEN 267
-#define WHILE 268
-#define CASE 269
-#define ESAC 270
-#define OF 271
-#define DARROW 272
-#define NEW 273
-#define ISVOID 274
-#define STR_CONST 275
-#define INT_CONST 276
-#define BOOL_CONST 277
-#define TYPEID 278
-#define OBJECTID 279
-#define ASSIGN 280
-#define NOT 281
-#define LE 282
-#define ERROR 283
-#define LET_STMT 285
+#define PROGRAM 258
+#define CLASS 259
+#define METHOD 260
+#define ATTR 261
+#define FORMAL 262
+#define BRANCH 263
+#define ASSIGN 264
+#define STATIC_DISPATCH 265
+#define DISPATCH 266
+#define COND 267
+#define LOOP 268
+#define TYPCASE 269
+#define BLOCK 270
+#define LET 271
+#define PLUS 272
+#define SUB 273
+#define MUL 274
+#define DIVIDE 275
+#define NEG 276
+#define LT 277
+#define EQ 278
+#define LEQ 279
+#define COMP 280
+#define INT 281
+#define STR 282
+#define BOOL 283
+#define NEW 284
+#define ISVOID 285
+#define NO_EXPR 286
+#define OBJECT 287
+#define NO_TYPE 288
+#define STR_CONST 289
+#define INT_CONST 290
+#define ID 291
+#define LINENO 292
 
 
 
 
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
 typedef union YYSTYPE
-#line 46 "cool.y"
+#line 22 "ast.y"
 {
+  int lineno;
   Boolean boolean;
   Symbol symbol;
   Program program;
@@ -161,30 +178,15 @@ typedef union YYSTYPE
   Cases cases;
   Expression expression;
   Expressions expressions;
-  char *error_msg;
 }
 /* Line 1489 of yacc.c.  */
-#line 124 "cool.tab.h"
+#line 140 "ast.tab.h"
 	YYSTYPE;
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
 # define YYSTYPE_IS_TRIVIAL 1
 #endif
 
-extern YYSTYPE cool_yylval;
+extern YYSTYPE ast_yylval;
 
-#if ! defined YYLTYPE && ! defined YYLTYPE_IS_DECLARED
-typedef struct YYLTYPE
-{
-  int first_line;
-  int first_column;
-  int last_line;
-  int last_column;
-} YYLTYPE;
-# define yyltype YYLTYPE /* obsolescent; will be withdrawn */
-# define YYLTYPE_IS_DECLARED 1
-# define YYLTYPE_IS_TRIVIAL 1
-#endif
-
-extern YYLTYPE cool_yylloc;
 #endif
